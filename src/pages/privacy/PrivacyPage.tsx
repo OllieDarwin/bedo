304b1e3c1490423825fcfaa5a316971d5e4f49c2
OllieDarwin <oliverdarwin365@gmail.com>
2025-03-05 22:38:34 +0000

    Added content to 'T&Cs' and 'Privacy Policy'.

--- src/pages/privacy/PrivacyPage.tsx
+++ src/pages/privacy/PrivacyPage.tsx
@@ -5,8 +5,49 @@ function PrivacyPage() {
     return (
         <>
             <Header/>
-            <section className="bg-[#030303] h-screen flex flex-col items-center justify-center">
+            <section className="bg-[#030303] flex flex-col justify-center items-center py-32">
                 <h1 className="text-3xl md:text-4xl font-bold mt-2 leading-12">Privacy Policy</h1>
+                <p className="text-zinc-500">Last updated: 05/03/25</p>
+                <div className="w-1/2 mt-8">
+                    <p>BEDO Intelligence is committed to protecting your privacy and ensuring the security of your personal data. This privacy policy outlines how we collect, use and protect your information in compliance with the <b>Computer Misuse Act (CMA) 1990, Copyright, Designs and Patents Act (CDPA) 1988, Data Protection Act (DPA) 1998 and Regulation of Investigatory Powers Act (RIPA) 2000.</b></p>
+                    
+                    <h2 className="mt-4 font-bold text-lg">1. Information We Collect</h2>
+                    <p>We may collect the following types of personal and non-personal data when you use our services:</p>
+                    <ul className="list-disc ml-4">
+                        <li><b>Personal Information: </b>Name, email address, phone number, payment details.</li>
+                        <li><b>Technical Data: </b>IP address, browser type, device information.</li>
+                        <li><b>Usage Data: </b>Interactions with our website, pages visited and preferences.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">2. How We Use Your Data</h2>
+                    <p>We use your information for:</p>
+                    <ul className="list-disc ml-4">
+                        <li>Providing and improving our AI-powered stock trading services.</li>
+                        <li>Processing payments and managing accounts.</li>
+                        <li>Ensuring security and fraud prevention.</li>
+                        <li>Complying with legal obligations.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">3. Data Protection & Security</h2>
+                    <ul className="list-disc ml-4">
+                        <li>We implement strong encryption and access control measures to prevent unauthorised access in line with the Computer Misuse Act 1990.</li>
+                        <li>We do not sell or share your data with third parties unless required by law or for essential services.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">4. Your Rights</h2>
+                    <p>Under the <b>Data Protection Act 1998</b> and relevant data protection laws, you have the right to:</p>
+                    <ul className="list-disc ml-4">
+                        <li>Request access to your data.</li>
+                        <li>Request correction or deletion of your personal information.</li>
+                        <li>Withdraw consent for data processing where applicable.</li>
+                    </ul>
+                    <p>To exercise these rights, contact us.</p>
+                    
+                    <h2 className="mt-4 font-bold text-lg">5. Cookies & Tracking Technologies</h2>
+                    <p>We use cookies to enhance user experiences and analyse website traffic. You can control cookie settings in your web browser.</p>
+                
+                    <p className="mt-4">For any privacy related concerns, contact us.</p>
+                </div>
             </section>
             <Footer />
         </>
--- src/pages/terms/TermsPage.tsx
+++ src/pages/terms/TermsPage.tsx
@@ -5,8 +5,51 @@ function TermsPage() {
     return (
         <>
             <Header/>
-            <section className="bg-[#030303] h-screen flex flex-col items-center justify-center">
-                <h1 className="text-3xl md:text-4xl font-bold mt-2 leading-12">Terms of Service</h1>
+            <section className="bg-[#030303] flex flex-col justify-center items-center py-32">
+                <h1 className="text-3xl md:text-4xl font-bold mt-2 leading-12">Terms & Conditions</h1>
+                <p className="text-zinc-500">Last updated: 05/03/25</p>
+                <div className="w-1/2 mt-8">
+                    <p>These Terms & Conditions govern your use of BEDO Intelligence's website and services. By accessing our website, you agree to comply with these terms.</p>
+                    
+                    <h2 className="mt-4 font-bold text-lg">1. Use of Our Services</h2>
+                    <ul className="list-disc ml-4">
+                        <li>You must be at least 18 years old to use our stock trading services.</li>
+                        <li>You agree not to engage in unauthorised access, hacking, or distribution of malware, in compliance with the <b>Computer Misuse Act 1990</b>.</li>
+                        <li>You shall not use our platform to distribute any material without permission, following the <b>Copyright, Designs and Patents Act 1988</b>.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">2. Intellectual Property</h2>
+                    <ul className="list-disc ml-4">
+                        <li>All content, AI models, and trading algorithms on BEDO Intelligence are our intellectual property. Unauthorised copying or distribution is prohibited under the <b>Copyright, Designs and Patents Act 1988</b>.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">3. Account Security</h2>
+                    <ul className="list-disc ml-4">
+                        <li>You are resposible for maintaining the confidentiality of your account credentials.</li>
+                        <li>Any suspected breach of security must be reported immediately to use via our contact information.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">4. Compliance with Legal Obligations</h2>
+                    <ul className="list-disc ml-4">
+                        <li>We may be required to share data with authorities under the <b>Regulation of Investigatory Powers Act 2000</b> when legally mandated.</li>
+                        <li>Any attempts to unlawfully access our systems will be prosecuted under the <b>Computer Misuse Act 1990</b>.</li>
+                    </ul>
+                    <p>To exercise these rights, contact us.</p>
+                    
+                    <h2 className="mt-4 font-bold text-lg">5. Limitation of Liability</h2>
+                    <ul className="list-disc ml-4">
+                        <li>BEDO Intelligence provides stock trading insights but does not guarantee financial success. We are not responsible for losses incurred while using our services.</li>
+                        <li>We do not take liability for system downtme, cyber-attacks or unauthorised account breaches.</li>
+                    </ul>
+
+                    <h2 className="mt-4 font-bold text-lg">6. Termination of Service</h2>
+                    <ul className="list-disc ml-4">
+                        <li>We reserve the right to suspend or terminate accounts found in violation of these terms.</li>
+                        <li>Users who attempt unauthorised access or distribute malicious software will be reported to authorities under the <b>Computer Misuse Act 1990</b>.</li>
+                    </ul>
+
+                    <p className="mt-4">For any questions, please contact us.</p>
+                </div>
             </section>
             <Footer />
         </>
